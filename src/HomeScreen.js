4b98ded6b9e5a5d0261b749845fbf0af6ba1680a
johnvibar <jmichael@captivatechat.com>
2023-09-14 23:31:03 -0500

    update home screen with po details

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,18 +1,77 @@
 import React, { useState, useEffect } from 'react'
-import { DataGrid } from '@mui/x-data-grid'
+import { DataGrid, GridEventListener } from '@mui/x-data-grid'
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '80%',
+    height: 600,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 const columns = [
     { field: 'CustomerPO', headerName: 'PO', width: 300 },
     { field: 'CustomerPODate', headerName: 'Customer PO Date', width: 200 },
-    { field: 'QtySo', headerName: 'Total Price', width: 200 }
+    { field: 'TotalQtySo', headerName: 'Total QtySo', width: 200 }
+]
+
+const poDetailColumns = [
+    { field: 'ConverterPONumber', headerName: 'PO Number', width: 100 },
+    { field: 'CustomerPODate', headerName: 'Customer PO Date', width: 100 },
+    { field: 'SOStatus', headerName: 'SO Status', width: 200 },
+    { field: 'QtySo', headerName: 'QTY SO', width: 200 },
+    { field: 'Price', headerName: 'Price', width: 200 },
+    { field: 'CustomerPartNumber', headerName: 'Customer Part Number', width: 200 },
+    { field: 'CustomerPNDescription', headerName: 'Customer P.N Desc', width: 200 },
+    { field: 'OCDate', headerName: 'OC Delivery Date', width: 200 },
+    { field: 'AmountDelivered', headerName: 'Amount Delivered', width: 200 },
+    { field: 'BalanceToDeliver', headerName: 'Balance to Deliver', width: 200 },
+    { field: 'UpdatedDeliveryDate', headerName: 'Updated Delivery Date', width: 200 }
 ]
 
 const HomeScreen = () => {
 
     const [tableData, setTableData] = useState([])
+    const [poDetails, setPoDetails] = useState([])
+
     const airtable_api_key = 'patsRqdsRWiw3Uwee.8e2ab165376c5a06db88f02ed66b08e7d2ff20c1ce6080495c5f1ef958fd65f4';
     const oauth_client_id = '6195b9e4-3576-4a34-8268-ce2ee4cf9222';
     const oauth_client_secret = 'fd45e0554b5a9b726085d685f6944c02a38839a864d68bf4941bc61ae7781ed4';
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen: GridEventListener<'rowClick'> = (
+        params, // GridRowParams
+        event, // MuiEvent<React.MouseEvent<HTMLElement>>
+        details, // GridCallbackDetails
+    ) => {
+        setPoDetails(params.row.PODetails)
+        console.log(`Item "${params.row.CustomerPO}" clicked`)
+        setOpen(true);
+    };
+    const handleClose = () => setOpen(false);
+
+    // Accepts the array and key
+    const groupBy = (array, key) => {
+        // Return the end result
+        return array.reduce((result, currentValue) => {
+            // If an array already present for key, push it to the array. Else create an array and push the object
+            if (!result[currentValue[key]]) {
+                result[currentValue[key]] = [];
+            }
+            result[currentValue[key]].push(currentValue);
+
+            // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
+            return result;
+        }, {}); // empty object is the initial value for result object
+    };
 
     useEffect(() => {
         fetch("https://api.airtable.com/v0/appdvY1q8ohEI0O9k/tblb2Xa0c5KA839Vj",
@@ -30,8 +89,18 @@ const HomeScreen = () => {
                     return {...row, id: record.id, RowCreatedTime: record.createdTime}
                 })
 
-                console.log(items)
-                setTableData(items)
+                // Group by color as key to the person array
+                const itemsGrouped = groupBy(items, 'CustomerPO')
+                const itemsDetailed = Object.entries(itemsGrouped).map(([key, value]) => {
+                    const sumOfQtySo = value.reduce((sum, row) => {
+                        return sum + row.QtySo;
+                    }, 0);
+
+                    return {id: key, CustomerPO: key, CustomerPODate: value[0]['CustomerPODate'], TotalQtySo: sumOfQtySo, PODetails: value}
+                })
+
+                console.log(itemsDetailed)
+                setTableData(itemsDetailed)
             })
 
     }, [])
@@ -39,13 +108,31 @@ const HomeScreen = () => {
     console.log(tableData)
 
     return (
-        <div style={{ height: 700, width: '100%' }}>
-            <DataGrid
-                rows={tableData}
-                columns={columns}
-                pageSize={10}
-            />
-        </div>
+        <Stack spacing={2} sx={{ width: '100%' }}>
+            <Box sx={{ height: 800, width: '100%' }}>
+                <DataGrid
+                    rows={tableData}
+                    columns={columns}
+                    pageSize={25}
+                    onRowClick={handleOpen} {...tableData}
+                />
+            </Box>
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <DataGrid
+                        rows={poDetails}
+                        columns={poDetailColumns}
+                        pageSize={25}
+                    />
+                </Box>
+            </Modal>
+        </Stack>
     )
 }
 
