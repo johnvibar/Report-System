5fe2d625f8692e6b5291a04e0c68bda96e3cf32a
johnvibar <jmichael@captivatechat.com>
2023-09-22 04:23:14 +0800

    009: date format

--- frontend/src/constants/column.constants.js
+++ frontend/src/constants/column.constants.js
@@ -1,12 +1,37 @@
 export const orderColumns = [
   { id: 1, field: "customerPO", headerName: "PO", width: 161 },
-  { id: 2, field: "customerPODate", headerName: "PO Date", width: 188 },
+  {
+    id: 2,
+    field: 'customerPODate',
+    headerName: 'PO Date',
+    width: 188,
+    renderCell: (params) => {
+      if (params.value === null || params.value === undefined) {
+        return '';
+      }
+      const date = new Date(params.value);
+      const options = { year: 'numeric', month: 'short', day: '2-digit' };
+      return date.toLocaleDateString('en-US', options);
+    },
+  },
   { id: 3, field: "TotalQtySo", headerName: "PO Value" },
 ];
 
 export const poDetailColumns = [
   { id: 1, field: "ConverterPONumber", headerName: "PO Number", width: 143 },
-  { id: 2, field: "CustomerPODate", headerName: "PO Date", width: 129 },
+  {
+    id: 2, field: "CustomerPODate",
+    headerName: "PO Date",
+    width: 129,
+    renderCell: (params) => {
+      if (params.value === null || params.value === undefined) {
+        return '';
+      }
+      const date = new Date(params.value);
+      const options = { year: 'numeric', month: 'short', day: '2-digit' };
+      return date.toLocaleDateString('en-US', options);
+    },
+  },
   { id: 3, field: "SOStatus", headerName: "SO Status", width: 129 },
   {
     id: 4, field: "CustomerPartNumber",
@@ -19,13 +44,34 @@ export const poDetailColumns = [
     width: 214,
   },
   { id: 6, field: "QtySo", headerName: "QTY SO", width: 129 },
-  { id: 7, field: "OCDate", headerName: "OC Delivery Date", width: 168 },
+  {
+    id: 7,
+    field: "OCDate",
+    headerName: "OC Delivery Date",
+    width: 168,
+    renderCell: (params) => {
+      if (params.value === null || params.value === undefined) {
+        return '';
+      }
+      const date = new Date(params.value);
+      const options = { year: 'numeric', month: 'short', day: '2-digit' };
+      return date.toLocaleDateString('en-US', options);
+    },
+  },
   { id: 8, field: "AmountDelivered", headerName: "Amount Delivered", width: 170 },
   { id: 9, field: "BalanceToDeliver", headerName: "Balance to Deliver", width: 165 },
   {
     id: 10, field: "UpdatedDeliveryDate",
     headerName: "Updated Delivery D.",
     width: 196,
+    renderCell: (params) => {
+      if (params.value === null || params.value === undefined) {
+        return '';
+      }
+      const date = new Date(params.value);
+      const options = { year: 'numeric', month: 'short', day: '2-digit' };
+      return date.toLocaleDateString('en-US', options);
+    },
   },
 ];
 
